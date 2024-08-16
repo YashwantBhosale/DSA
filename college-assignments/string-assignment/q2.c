120c65cc681349c4c2cd03138ca7c0bb47fe67c4
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2024-08-17 01:11:02 +0530

    str tok

--- college-assignments/string-assignment/q2.c
+++ college-assignments/string-assignment/q2.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
- 
+char *str_tok(char *str, char *delim) {
+    static char *ptr = NULL;
+    if(str)
+        ptr = str;
+    if(ptr == NULL)
+        return NULL;
+    char *start = ptr;
+    char *p = ptr;
+    while(*p != '\0' && *p != *delim)
+        p++;
+    if(*p == '\0'){
+        ptr = NULL;
+    }
+    else {
+        *p = '\0';
+        ptr = ++p;
+    }
+    return start;
+}
+
 
 int main() {
     char string[64];
     char *token;
-    strcpy(string, "hello,world");
+    strcpy(string, "yashwant,chandrakant,bhosale, 1234");
 
-    // Get the first token
     token = str_tok(string, ",");
 
-    // Print each token
     while (token != NULL) {
         printf("%s\n", token);
         token = str_tok(NULL, ",");
