37c0b0860b2b33c4b6d6eec1b00efa1ee3a09a0b
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2024-08-17 15:32:32 +0530

    longest possible subsequence

---
+++ college-assignments/string-assignment/q3.c
@@ -0,0 +1,41 @@
+//  Write a function which finds the longest possible subsequence of one string into another and returns the length + pointer to the subsequence.
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int len;
+    char *seq;
+} result;
+
+result *str(char *str1, char *str2) {
+    char *p = str1;
+    int max = 0;
+    char *max_seq = NULL;
+    while(*p != '\0') {
+        int count = 0;
+        while(p[count] == str2[count]){
+            count++;
+        }
+        if(count > max) {
+            max = count;
+            max_seq = p;
+        }
+
+        count = 0;
+        p++;
+    }
+
+    result *return_result = (result *) malloc(sizeof(result));
+    return_result -> len = max;
+    return_result -> seq = max_seq;
+    return return_result;
+
+}
+
+int main() {
+    char str1[] = "abcdemnopxyz";
+    char str2[] = "mnotq";
+    result *p = str(str1, str2);
+    printf("Longest subsequence: %s,\ncharacters matched: %d\n", p->seq, p->len);
+    return 0;
+}
