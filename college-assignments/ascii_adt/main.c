da54487ba09c3b8dd2cdba558d8fc7aa813a5bfe
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2024-09-01 12:02:54 +0530

    update: ascii adt

--- college-assignments/ascii_adt/main.c
+++ college-assignments/ascii_adt/main.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
 #include "ascii.h"
 
 int main() {
     list l;
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
     init_ascii(&l);
-    ascii_of(&l, 'A');
+    ascii_of(&l, c);
     traverse(l);
     destroy(&l);
     traverse(l);
