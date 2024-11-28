488be4f9c2db670b9c43249112e468f55a2cd6f3
YashwantBhosale <yashwantbhosale07@gmail.com>
2024-11-29 01:53:44 +0530

    add: hashing assingment Que 1. simple hash table implementation with hash function using division method

---
+++ college-assignments/hashing-assignment/1_Hash_functions/hash_table.h
@@ -0,0 +1,28 @@
+// Hash table functions
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    EXPERIMENT:
+    Implement a simple hash function using the division method.
+    Write a small program that hashes integers using your function, then observe
+    how data is distributed across an array of a fixed size.
+*/
+
+typedef struct {
+    int key;
+    int value;
+} Entry;
+
+typedef struct {
+    Entry **table;
+    int size;
+} HashTable;
+
+void init_table(HashTable *ht, int size);
+void insert(HashTable *ht, int key, int value);
+void delete(HashTable *ht, int key);
+int search(HashTable *ht, int key);
+void print_table(HashTable *ht);
+void free_table(HashTable *ht);
+int hash(int key, int size);
